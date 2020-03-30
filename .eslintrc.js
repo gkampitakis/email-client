3e291ee7bf16b6fa917d420d555fb29f3388f1ab
George Kampitakis <gkabitakis@gmail.com>
2020-03-30 20:49:14 +0100

    Move from lint to eslint and update dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+	parser: '@typescript-eslint/parser',
+	extends: ['plugin:@typescript-eslint/recommended'],
+	plugins: ['@typescript-eslint'],
+	rules: {
+		'sort-imports': [
+			'error',
+			{
+				ignoreCase: false,
+				ignoreDeclarationSort: true,
+				ignoreMemberSort: false,
+				memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single']
+			}
+		],
+		'no-multiple-empty-lines': [
+			'error',
+			{
+				max: 2,
+				maxEOF: 1
+			}
+		],
+		'@typescript-eslint/ban-ts-ignore': ['off'],
+		'@typescript-eslint/interface-name-prefix': ['error', 'never'],
+		'@typescript-eslint/explicit-function-return-type': ['off'],
+		'@typescript-eslint/no-parameter-properties': ['off'],
+		'@typescript-eslint/member-ordering': ['error', { default: ['field', 'constructor', 'method'] }],
+		'comma-dangle': ['error', 'never'],
+		'no-unused-expressions': ['off'],
+		'sort-keys': ['off'],
+		'max-len': [
+			'error',
+			{
+				code: 150,
+				tabWidth: 4
+			}
+		],
+		'object-shorthand': ['off'],
+		radix: ['off'],
+		'no-console': [1],
+		'no-shadow': ['off'],
+		'prefer-const': 2,
+		semi: [2, 'always']
+	}
+};
